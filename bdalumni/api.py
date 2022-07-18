470d01a31c424b83b01f8391fdef611f45d0b588
Alejandro Aguirre <aaguirre@ucsm.edu.pe>
2022-07-18 18:48:15 +0200

    dsa

--- bdalumni/api.py
+++ bdalumni/api.py
@@ -9,3 +9,9 @@ def docField(docname):
 def docTable(docname):
 	data = frappe.db.sql("""SELECT * FROM tabDocField WHERE parent = %(docname)s  AND in_list_view = 1  ORDER BY idx ASC;""", values={"docname":docname}, as_dict=1)
 	return data
+
+
+@frappe.whitelist(allow_guest=True)
+def docListView(docname):
+	data = frappe.db.sql("""SELECT * FROM tabDocField WHERE parent = %(docname)s  AND in_list_view = 1  ORDER BY idx ASC;""", values={"docname":docname}, as_dict=1)
+	return data
