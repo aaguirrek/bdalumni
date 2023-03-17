5fa53a0f54a220f48f6f2978d5ecdef948031fac
alejandro <support@peruintercorp.com>
2023-03-17 22:58:58 +0100

    ds

--- setup.py
+++ setup.py
@@ -1,19 +1,6 @@
-from setuptools import setup, find_packages
+# TODO: Remove this file when bench >=v5.11.0 is adopted / v15.0.0 is released
+from setuptools import setup
 
-with open("requirements.txt") as f:
-	install_requires = f.read().strip().split("\n")
+name = "bdalumni"
 
-# get version from __version__ variable in bdalumni/__init__.py
-from bdalumni import __version__ as version
-
-setup(
-	name="bdalumni",
-	version=version,
-	description="Evolution Soluciones",
-	author="Evolution Soluciones",
-	author_email="avelazco@evolutionsoluciones.com",
-	packages=find_packages(),
-	zip_safe=False,
-	include_package_data=True,
-	install_requires=install_requires
-)
+setup()
