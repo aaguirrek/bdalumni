18fa4180da72837141db4c2a57119b21c65a293c
alejandro <support@peruintercorp.com>
2023-09-19 17:11:21 -0500

    dsa

---
+++ bdalumni/bdalumni/doctype/puestos_gestor/test_puestos_gestor.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Evolution Soluciones and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestPuestosgestor(FrappeTestCase):
+	pass
