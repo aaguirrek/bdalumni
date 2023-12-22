322e16e3d32661fc215b49002a7064b5860ffe4c
alejandro <support@peruintercorp.com>
2023-12-22 12:05:58 -0500

    Revert "ola"
    This reverts commit 05bfdc4567c1d2a505f16a03e77e4cc177592cb8.

--- bdalumni/bdalumni/doctype/sectores_gestor/test_sectores_gestor.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2023, Evolution Soluciones and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestSectoresGestor(FrappeTestCase):
-	pass
